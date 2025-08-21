5c908db8ed85d1b1c4ebb63635bd8d4340909e96
Thiago Figueiredo <tyagohenryque2014@gmail.com>
2025-08-21 18:54:22 -0300

    Feat: Adiciona interface de linha de comando com Typer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # main.py
+import typer
 import sys
 import os
 
@@ -7,11 +8,19 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from core.conversor import converter_usd_para_brl
 from infrastructure.ui import exibir_resultado
 
-if __name__ == "__main__":
+app = typer.Typer()
 
-    valor_dolar = 100
-    contacao_atual = 5.0
-
-    valor_em_real = converter_usd_para_brl(valor_dolar, contacao_atual)
-
+@app.command()
+def converter(
+    valor_em_dolar: float = typer.Argument(..., help="O valor em dólares a ser convertido."),
+    cotacao: float = typer.Option(5.0, "--cotacao", "-c", help="A cotação atual do dólar para a conversão.")
+):
+    """
+    Converte um valor de Dólar para Real com base em uma cotação.
+    """
+    valor_em_real = converter_usd_para_brl(valor_em_dolar, cotacao)
     exibir_resultado(valor_em_real)
+
+
+if __name__ == "__main__":
+    app()
